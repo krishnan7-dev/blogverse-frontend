67ed7dcf60d1f973defdb1ed3724430114c922d7
Adarsh <agkrishnan2003@gmail.com>
2022-08-22 21:15:48 +0530

    Updated UI

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Importing components
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Blogs from './components/Blogs';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+      <div className="App">
+        <Router>
+          <Navbar/>
+          <Routes>
+            <Route exact path="/" element={ <Home/> }></Route>
+            <Route exact path="/blogs" element={ <Blogs/> }></Route>
+            <Route exact path="/login" element={ <Login/> }></Route>
+            <Route exact path="/signup" element={ <Signup/> }></Route>
+          </Routes>
+          <Footer/>
+        </Router>
+      </div>
+  );
+}
+
+export default App;
