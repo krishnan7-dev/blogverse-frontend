57575025894dc90759f4f43e24505ed481185c9a
Adarsh <agkrishnan2003@gmail.com>
2022-08-23 20:01:13 +0530

    Authorized routes for user

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,16 @@ import Logo from '../images/logo.svg';
 import { Link } from "react-router-dom";
 import NavLink from "./NavLink";
 
+const { useJwt } = require('react-jwt');
+
 const Navbar = () => {
+    const accessToken = localStorage.getItem('accessToken');
+    const { decodedToken, isExpired } = useJwt(accessToken);
+
+    console.log(decodedToken);
+    console.log(isExpired);
+    (decodedToken && !isExpired) ? console.log('Logged in') : console.log('Not logged in');
+
     return (
         <nav id="navbar">
             <Link to="/">
@@ -11,12 +20,20 @@ const Navbar = () => {
                 </div>
             </Link>
             <div className="links">
-                <ul>
-                    <Link to="/"><NavLink text="Home" /></Link>
-                    <Link to="/blogs"><NavLink text="Blogs" /></Link>
-                    <Link to="/login"><NavLink text="Login" /></Link>
-                    <Link to="/signup"><NavLink text="Sign Up" /></Link>
-                </ul>
+                { (decodedToken && !isExpired) ? (
+                    <ul>
+                        <Link to="/create"><NavLink text="Create" /></Link>
+                        <Link to="/blogs"><NavLink text="Blogs" /></Link>
+                        <Link to="/profile"><NavLink text="Your Profile" /></Link>
+                    </ul>
+                ) : (
+                    <ul>
+                        <Link to="/"><NavLink text="Home" /></Link>
+                        <Link to="/blogs"><NavLink text="Blogs" /></Link>
+                        <Link to="/login"><NavLink text="Login" /></Link>
+                        <Link to="/signup"><NavLink text="Sign Up" /></Link>
+                    </ul>
+                ) }
             </div>
         </nav>
     );
