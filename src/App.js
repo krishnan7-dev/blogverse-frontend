13181cefa4dbdc9b6cf6a021843ad2ed914e2604
Adarsh <agkrishnan2003@gmail.com>
2022-08-24 09:05:16 +0530

    Added create and logout routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+
 // Importing components
 import Navbar from './components/Navbar';
 import Home from './components/Home';
@@ -8,6 +9,7 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import Footer from './components/Footer';
 import Profile from './components/Profile';
+import Create from './components/Create';
 
 function App() {
   return (
@@ -19,7 +21,9 @@ function App() {
             <Route exact path="/blogs" element={ <Blogs/> }></Route>
             <Route exact path="/login" element={ <Login/> }></Route>
             <Route exact path="/signup" element={ <Signup/> }></Route>
-            <Route path='/profile' element={ <Profile/> }></Route>
+            <Route path="/profile" element={ <Profile/> }></Route>
+            <Route path="/create" element={ <Create/> }></Route>
+            <Route exact path="/logout" element={ <Login/> }></Route>
           </Routes>
           <Footer/>
         </Router>
