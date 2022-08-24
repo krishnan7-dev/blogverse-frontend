848ccc57c88dbe4783cff1eba524f988574e2e24
Adarsh <agkrishnan2003@gmail.com>
2022-08-24 22:47:08 +0530

    Create blog component

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,6 +1,15 @@
 const Create = () => {
     return (
-        <div>Create</div>
+        <div className="create">
+            <div className="create-container">
+                <h1 className="create-title">Create Blog</h1>
+                <form>
+                    <input type="text" placeholder="Blog Title" />
+                    <textarea name="" cols="30" rows="10" placeholder="Write Blog Here..."></textarea>
+                </form>
+                <button>Publish</button>
+            </div>
+        </div>
     );
 }
  
